3c5e1505e7450b9f2989ebdb049eb69caf94bb35
Chris Miller <chris@chrmi.com>
2018-01-18 15:44:20 -0800

    More cars and visual tweaks

---
+++ Assets/Scripts/ExternalMessenger.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Runtime.InteropServices;
+
+public class ExternalMessenger : MonoBehaviour {
+    #if UNITY_WEBGL
+    [DllImport("__Internal")]
+    private static extern void FinishLoading();
+    #endif
+
+    void Start()
+    {
+        #if UNITY_WEBGL
+        FinishLoading();
+        #endif
+    }
+}
